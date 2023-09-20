2f35d1bea6a48e085673987a1febc2f7f285391f
KaueTeixxeira <kaueteixerak9@hotmail.com>
2023-09-20 16:58:27 -0300

    Iniciando setup do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+import Search from './pages/Search'
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie/:id" element={<Movie/>}/>
+        <Route path="/search" element={<Search/>}/>
+        </Routes>
+      </BrowserRouter>
+      
     </>
   )
 }
